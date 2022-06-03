a8bef3b8534b197e6a4edf402daa7b5c43252ad9
bugpz <13558622779@163.com>
2022-06-03 11:55:00 +0800

    chore：增加代理配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    open: '/',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:10086',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '/api')
+      }
+    },
+    port: 3000
+  }
 })
