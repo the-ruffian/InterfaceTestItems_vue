c04144f1892c161af994f618eb9a7666c5bf04e5
bugpz <13558622779@163.com>
2022-06-03 14:41:32 +0800

    fix：加载依赖包

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from '@vue/runtime-dom'
+import router from './router'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import regular_check from "./common/regular_check";
 // @ts-ignore
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(router)
+    .use(Antd)
+    .use(regular_check)
+    .mount('#app')
