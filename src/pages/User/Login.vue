ac23125fd2b04333e21d4dd0a3d53d290a86761c
bugpz <13558622779@163.com>
2022-06-04 22:23:48 +0800

    fix：修复bug，完善登录判断

--- src/pages/User/Login.vue
+++ src/pages/User/Login.vue
@@ -8,7 +8,6 @@ import {message} from "ant-design-vue";
 
 
 const internalInstance = getCurrentInstance();
-const re = internalInstance?.appContext.config.globalProperties
 
 
 const form = reactive({
@@ -16,46 +15,62 @@ const form = reactive({
   password: ''
 })
 const checkPhone = (rule:RuleObject, value:number) => {
-  if (!re.$checkPhone.test(value)){
+  if (!internalInstance?.appContext.config.globalProperties.$checkPhone.test(value)){
     return Promise.reject('请输入正确的手机号')
   } else {
     return Promise.resolve()
   }
 }
 const checkPassword = (rule:RuleObject, value:string) => {
-  if (!re.$checkPassword.test(value)){
+  if (!internalInstance?.appContext.config.globalProperties.$checkPassword.test(value)){
     return Promise.reject('密码8-20位，支持英文、数字')
   } else {
     return Promise.resolve()
   }
 }
 const login = () => {
-  if (re.$checkPhone.test(form.phone)
-    && re.$checkPassword.test(form.password)){
+  console.log(form)
+  console.log(internalInstance?.appContext.config.globalProperties.$checkPhone.test(form.phone))
+  console.log(internalInstance?.appContext.config.globalProperties.$checkPhone.test(form.password))
+
+  if (internalInstance?.appContext.config.globalProperties.$checkPhone.test(form.phone)
+    && internalInstance?.appContext.config.globalProperties.$checkPassword.test(form.password)){
     userLogin({
       phone: form.phone,
       password: Md5.hashStr(form.password),
     })
         .then(res => {
-          if (res.data.code === 200){
+          console.log(res.data)
+          if (res.data.message === '登录成功'){
             const token = res.data.result.token
             localStorage.setItem('token',token)
             localStorage.setItem('phone', form.phone)
             localStorage.setItem('username', res.data.result.username)
             localStorage.setItem('is_login', 'true')
+            // location.replace('/')
           } else {
             message.error(res.data.message)
             setTimeout(function () {location.reload()}, 1500)
           }
         })
-  }else {
-    message.error('请先填写账号、密码')
+  }else if (form.phone == ''){
+    message.error('请填写账号')
+  }else if (form.password == ''){
+    message.error('请填写密码')
+  } else {
+    message.error('请填写正确的账号、密码')
   }
 }
 
 const rules = {
   phone:[{required: true, validator: checkPhone, trigger: 'blur'}],
   password: [{required: true, validator: checkPassword, trigger: 'blur'}]
+}
+
+// onMounted  进页面时执行
+{
+  localStorage.clear()
+  sessionStorage.clear()
 }
 
 </script>
